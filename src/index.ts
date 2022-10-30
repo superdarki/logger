29c4da4a4fd72ee0902fc04c13d793993204a514
Lucas Delolme <lucasd0104@gmail.com>
2022-10-30 14:59:23 +0100

    fix: reorder exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+export * from './lib/color';
+export * from './lib/level';
 export * from './lib/interfaces';
 export * from './lib/logger';
 export * from './lib/loggerManager';
-export * from './lib/color';
-export * from './lib/level';
